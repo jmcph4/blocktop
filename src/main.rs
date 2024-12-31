bae9c1ba34f56b09d1f5aa8af062b860182431a9
jmcph4 <jack@jmcph4.dev>
2025-01-01 09:24:51 +1000

    Add TUI

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
 use clap::Parser;
+use crossterm::event::{self, Event};
+use ratatui::{DefaultTerminal, Frame};
 
 use crate::cli::Opts;
 
 pub mod cli;
 
+fn run(mut terminal: DefaultTerminal) -> eyre::Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if matches!(event::read()?, Event::Key(_)) {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
+}
+
 fn main() -> eyre::Result<()> {
     pretty_env_logger::init_timed();
     let _opts: Opts = Opts::parse();
-    Ok(())
+
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+
+    result
 }
