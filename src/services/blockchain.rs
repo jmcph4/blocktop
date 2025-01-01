ba0c4bd7043902ca2b45673434a75b2676ed2015
jmcph4 <jack@jmcph4.dev>
2025-01-01 11:41:45 +1000

    Store latest known block hash

--- src/services/blockchain.rs
+++ src/services/blockchain.rs
@@ -7,12 +7,21 @@ use futures::StreamExt;
 use tokio::runtime::Builder;
 use url::Url;
 
+use alloy::primitives::B256;
+
 use crate::client::{Client, WsClient};
 
 const NUM_WORKERS: usize = 1;
 
+#[derive(Clone, Debug, Default)]
+pub struct HeadInfo {
+    pub latest_block_hash: Option<B256>,
+}
+
+#[derive(Clone, Debug)]
 pub struct BlockchainService {
     client: WsClient,
+    info: HeadInfo,
 }
 
 impl BlockchainService {
@@ -25,12 +34,14 @@ impl BlockchainService {
                 .unwrap();
 
             runtime.block_on(async {
-                let this = Self {
+                let mut this = Self {
                     client: WsClient::new(rpc).await?,
+                    info: HeadInfo::default(),
                 };
                 while let Some(header) =
                     this.client.block_headers().await?.next().await
                 {
+                    this.info.latest_block_hash = Some(header.hash);
                     std::fs::OpenOptions::new()
                         .append(true)
                         .create(true)
@@ -44,5 +55,9 @@ impl BlockchainService {
                 Ok(this)
             })
         })
+    }
+
+    pub fn latest_block_hash(&self) -> Option<B256> {
+        self.info.latest_block_hash
     }
 }
