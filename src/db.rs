84c305e28f3e7119a74e714bb89d4397b73fb23c
jmcph4 <jack@jmcph4.dev>
2025-01-01 14:49:40 +1000

    Implement latest block header retrieval

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,7 @@
 use std::{path::PathBuf, sync::Arc, time::Duration};
 
 use alloy::{primitives::B256, rpc::types::eth::Header};
+use eyre::eyre;
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 
@@ -45,13 +46,23 @@ impl Database {
         })
     }
 
-    pub fn latest_block_hash(&self) -> Option<B256> {
-        todo!()
+    pub fn latest_block_hash(&self) -> eyre::Result<Option<B256>> {
+        match self.conn_pool.get()?.query_row(
+            "SELECT hash FROM block_headers ORDER BY inserted_at DESC LIMIT 1",
+            [],
+            |row| row.get::<usize, String>(1),
+        ) {
+            Ok(t) => Ok(Some(t.parse()?)),
+            Err(e) => match e {
+                rusqlite::Error::QueryReturnedNoRows => Ok(None),
+                _ => Err(eyre!(e)),
+            },
+        }
     }
 
     pub fn add_block_header(&self, header: &Header) -> eyre::Result<usize> {
         Ok(self.conn_pool.get()?.execute(
-            "INSERT INTO block_headers (hash) VALUES (?1)",
+            "INSERT INTO block_headers (inserted_at, hash) VALUES (now, ?1)",
             [header.hash.to_string()],
         )?)
     }
