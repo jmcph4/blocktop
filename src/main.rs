0454846c8d6be1a944ace05e25f430ad38feee9d
jmcph4 <jmcph4.github@gmail.com>
2025-01-06 14:32:29 +1000

    Add headless mode warning

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use clap::Parser;
-use db::{Database, Location};
-use services::blockchain::BlockchainService;
+use log::warn;
 
-use crate::{cli::Opts, ui::run};
+use crate::{
+    cli::Opts,
+    db::{Database, Location},
+    services::blockchain::BlockchainService,
+    ui::run,
+};
 
 pub mod cli;
 pub mod client;
@@ -16,7 +20,7 @@ fn main() -> eyre::Result<()> {
     let opts: Opts = Opts::parse();
 
     let db: Database = Database::new(match opts.db {
-        Some(file) => Location::Disk(file),
+        Some(ref file) => Location::Disk(file.to_path_buf()),
         None => Location::Memory,
     })?;
     let blockchain = BlockchainService::spawn(opts.rpc, db.clone());
@@ -27,6 +31,10 @@ fn main() -> eyre::Result<()> {
         ratatui::restore();
         result
     } else {
+        if opts.db.is_none() {
+            warn!("Headless mode without specifying an on-disk database. All data will be lost on exit.");
+        }
+
         let _ = blockchain.join();
         Ok(())
     }
