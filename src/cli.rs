026a648c40f1a92463abe12b751d50ded9c9698b
jmcph4 <jack@jmcph4.dev>
2025-01-17 11:02:55 +1000

    Add clap meta

--- src/cli.rs
+++ src/cli.rs
@@ -3,7 +3,9 @@ use std::path::PathBuf;
 use clap::Parser;
 use url::Url;
 
+/// Minimalist TUI block explorer and chain indexer
 #[derive(Clone, Debug, Parser)]
+#[clap(version, about, author)]
 pub struct Opts {
     #[clap(short, long, default_value = "wss://eth.merkle.io")]
     pub rpc: Url,
